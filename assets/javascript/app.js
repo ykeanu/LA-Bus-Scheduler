06a5b1536e0172ae25bb3faf797a652c473c8bc7
ykeanu <ykhernan@ucla.edu>
2017-10-04 15:51:57 -0700

    Updated html + linked to firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// -------------------- I. VARIABLES --------------------
+
+// -------------------- II. FUNCTIONS -------------------
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDIOUNKqe4Kl5j1E2zGQCW2OOt0qfqUugU",
+    authDomain: "bus-scheduler-f4f18.firebaseapp.com",
+    databaseURL: "https://bus-scheduler-f4f18.firebaseio.com",
+    projectId: "bus-scheduler-f4f18",
+    storageBucket: "bus-scheduler-f4f18.appspot.com",
+    messagingSenderId: "86167308000"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+// -------------------- III. MAIN PROCSS --------------------
+
+
+// PSUEDO CODE:
