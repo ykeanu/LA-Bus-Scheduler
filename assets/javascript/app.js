1aef01dca99ee94eda56c189ef2c7d4662443510
ykeanu <ykhernan@ucla.edu>
2017-10-04 16:47:47 -0700

    Updated basic functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-// -------------------- I. VARIABLES --------------------
-
-// -------------------- II. FUNCTIONS -------------------
+// -------------------- I. VARIABLES + DATABASE--------------------
 
 // Initialize Firebase
 var config = {
@@ -13,11 +11,80 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Store firebase in a variable
 var database = firebase.database();
+
+
+// -------------------- II. FUNCTIONS -------------------
+
+// Diplays current time
+$("#current-time").html(moment().format("hh:mm"));
+
 
 
 
 // -------------------- III. MAIN PROCSS --------------------
 
+// Send data to firebase
+$("#submit-btn").on("click", function(event) {
+    event.preventDefault();
 
-// PSUEDO CODE:
+    // Stores user input
+    var busName = $("#bus-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstBus = $("#first-bus-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+    // Convert to an object
+    var newBus = {
+        busName: busName,
+        destination: destination,
+        firstBus: firstBus,
+        frequency: frequency
+    };
+
+    database.ref().push(newBus);
+
+    //TESTING
+    console.log(newBus);
+
+
+    $("#bus-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-bus-input").val("");
+    $("#frequency-input").val("");
+
+    // Since button type is submit, prevent page from refreshing
+    return false;
+
+});
+
+database.ref().on("child_added", function(snapshot, key) {
+    console.log(snapshot.val());
+
+    var busName = snapshot.val().busName;
+    var destination = snapshot.val().destination;
+    var firstBus = snapshot.val().firstBus;
+    var frequency = snapshot.val().frequency;
+
+    var timeConverted = moment(firstBus, "hh:mm").subtract(1, "years");
+    var currentTime = moment();
+    var differenceTime = moment().diff(moment(timeConverted), "minutes");
+    var remainingTime = differenceTime % frequency;
+    var minutesRemaining = frequency - remainingTime;
+    var nextBus = moment().add(minutesRemaining, "minutes");
+    var nextArrival = moment(nextBus).format("hh:mm");
+
+    $("#bus-table > tbody")
+        .append("<tr><td>" +
+            busName + "</td><td>" +
+            destination + "</td><td>" +
+            frequency + "</td><td>" +
+            nextArrival + "</td><td>" +
+            minutesRemaining + "</td></tr>");
+
+
+    //TESTING
+    console.log(timeConverted);
+
+});
